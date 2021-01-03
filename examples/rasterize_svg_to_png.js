911507df8e81b5434b79c4c9445b77f31f41b9db
Mikael Finstad <finstaden@gmail.com>
2021-01-03 23:06:43 +0100

    add example of svg to png

---
+++ examples/rasterize_svg_to_png.js
@@ -0,0 +1,36 @@
+// Run this file as:
+//
+//   env TRANSLOADIT_KEY=xxx TRANSLOADIT_SECRET=yyy node rasterize_svg_to_png.js ./fixtures/circle.svg
+//
+// You'll likely just want to `require('transloadit')`, but we're requiring the local
+// variant here for easier testing:
+const TransloaditClient = require('../src/TransloaditClient')
+
+const transloadit = new TransloaditClient({
+  authKey   : process.env.TRANSLOADIT_KEY,
+  authSecret: process.env.TRANSLOADIT_SECRET,
+})
+
+transloadit.addFile('my_file', process.argv[2]);
+
+(async () => {
+  try {
+    const opts = {
+      waitForCompletion: true,
+      params           : {
+        steps: {
+          png: {
+            use   : ':original',
+            robot : '/image/resize',
+            format: 'png',
+          },
+        },
+      },
+    }
+
+    const status = await transloadit.createAssemblyAsync(opts)
+    console.log('Your PNG file:', status.results.png[0].url)
+  } catch (err) {
+    console.error(`createAssembly ${err.assembly_id} failed`, err)
+  }
+})()
