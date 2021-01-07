d216a5b10bbd70c4b3be450cfdaa618665405178
Mikael Finstad <finstaden@gmail.com>
2021-01-07 13:33:04 +0100

    fix lint

--- test/get-coverage-percent.js
+++ test/get-coverage-percent.js
@@ -1,4 +1,4 @@
-const json = require('../coverage/coverage-summary.json');
+const json = require('../coverage/coverage-summary.json')
 
 const avgPercentage = ['lines', 'statements', 'functions', 'branches'].map((type) => json.total[type].pct).reduce((acc, v) => acc + v, 0) / 4
 console.log(`${Math.round(avgPercentage)}%`)
