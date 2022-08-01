96381473e36e9a985a4971c3fe03f23b57b91be4
Mikael Finstad <finstaden@gmail.com>
2022-08-01 20:09:23 +0200

    fixes
    also pull out logic

---
+++ test/testserver.js
@@ -0,0 +1,103 @@
+const http = require('http')
+const got = require('got')
+
+const createTunnel = require('./tunnel')
+
+async function startTestServer (handler2) {
+  let customHandler
+
+  function handler (...args) {
+    if (customHandler) return customHandler(...args)
+    return handler2(...args)
+  }
+
+  const server = http.createServer(handler)
+
+  // Find a free port to use
+  let port = 8000
+  await new Promise((resolve, reject) => {
+    server.on('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        if (++port >= 65535) {
+          server.close()
+          return reject(new Error('Failed to bind to port'))
+        }
+        return server.listen(port, '127.0.0.1')
+      }
+      return reject(err)
+    })
+
+    server.listen(port, '127.0.0.1')
+
+    server.on('listening', resolve)
+  })
+
+  let tunnel
+  try {
+    tunnel = createTunnel({ cloudFlaredPath: process.env.CLOUDFLARED_PATH, port })
+
+    // eslint-disable-next-line no-console
+    tunnel.process.on('error', console.error)
+    tunnel.process.on('close', () => {
+      // console.log('tunnel closed')
+      server.close()
+    })
+
+    // console.log('waiting for tunnel to be created')
+    const url = await tunnel.urlPromise
+    // console.log('tunnel created', url)
+
+    try {
+      let curPath
+      let done = false
+
+      const promise1 = new Promise((resolve) => {
+        customHandler = (req, res) => {
+          // console.log('handler', req.url)
+
+          if (req.url !== curPath) throw new Error(`Unexpected path ${req.url}`)
+
+          done = true
+          res.end()
+          resolve()
+        }
+      })
+
+      const promise2 = (async () => {
+        // try connecting to the tunnel and resolve when connection successfully passed through
+        for (let i = 0; i < 10; i += 1) {
+          if (done) return
+          curPath = `/check${i}`
+          try {
+            await got(`${url}${curPath}`, { timeout: { request: 2000 } })
+            return
+          } catch (err) {
+            // console.error(err.message)
+            // eslint-disable-next-line no-shadow
+            await new Promise((resolve) => setTimeout(resolve, 3000))
+          }
+        }
+        throw new Error('Timed out checking for a functioning tunnel')
+      })()
+
+      await Promise.all([promise1, promise2])
+    } finally {
+      customHandler = undefined
+    }
+
+    // console.log('Tunnel ready')
+
+    return {
+      url,
+      close: () => tunnel.close(),
+    }
+  } catch (err) {
+    if (tunnel) tunnel.close()
+    server.close()
+    throw err
+  }
+}
+
+module.exports = {
+  startTestServer,
+}
--- test/tunnel.js
+++ test/tunnel.js
@@ -4,18 +4,15 @@ const { Resolver } = require('dns')
 const { promisify } = require('util')
 
 module.exports = ({ cloudFlaredPath = 'cloudflared', port }) => {
-  console.log('starting tunnel', port)
   const process = execa(cloudFlaredPath, ['tunnel', '--url', `http://localhost:${port}`, '--no-autoupdate'], { buffer: false, stdout: 'ignore' })
   const rl = readline.createInterface({ input: process.stderr })
 
   process.on('error', (err) => console.error(err))
 
   const urlPromise = (async () => {
-    console.log('finding url')
     const url = await new Promise((resolve) => {
       let foundUrl
       rl.on('line', (line) => {
-        console.log('line', line)
         if (!foundUrl) {
           const match = line.match(/(https:\/\/[^.]+\.trycloudflare\.com)/)
           if (!match) return
@@ -26,26 +23,26 @@ module.exports = ({ cloudFlaredPath = 'cloudflared', port }) => {
         }
       })
     })
-    console.log('found url')
+    // console.log('Found url')
 
     // We need to wait for DNS to be resolvable.
-    // If we don't, the operating system dns cache will be poisoned by the not yet valid dns
-    // and forever fail for that subdomain name
+    // If we don't, the operating system's dns cache will be poisoned by the not yet valid resolved entry
+    // and it will forever fail for that subdomain name...
     const resolver = new Resolver()
     resolver.setServers(['8.8.8.8']) // if we don't explicitly specify DNS server, it will also poison the OS dns cache
     const resolve4 = promisify(resolver.resolve4.bind(resolver))
     for (let i = 0; i < 10; i += 1) {
       try {
         const host = new URL(url).hostname
-        console.log('checking dns', host)
+        // console.log('checking dns', host)
         await resolve4(host)
         return url
       } catch (err) {
-        console.error(err.message)
+        // console.error(err.message)
         await new Promise((resolve) => setTimeout(resolve, 3000))
       }
     }
-    throw new Error('Timed out trying to resolve tunnel host')
+    throw new Error('Timed out trying to resolve tunnel dns')
   })()
 
   function close () {
