4e47cae6b746b271ef19c9c4f45bc818f574118b
Mikael Finstad <finstaden@gmail.com>
2023-11-07 23:39:37 +0900

    fix lint

--- test/tunnel.js
+++ test/tunnel.js
@@ -50,7 +50,7 @@ module.exports = ({ cloudFlaredPath = 'cloudflared', port }) => {
         if (!foundUrl) {
           const match = line.match(/(https:\/\/[^.]+\.trycloudflare\.com)/)
           if (!match) return
-            ;[, foundUrl] = match
+          ;[, foundUrl] = match
         } else {
           const match = line.match(
             /Connection [^\s+] registered connIndex=[^\s+] ip=[^\s+] location=[^\s+]/
