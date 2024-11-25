a12d2a9b5d0ddd2a234ebc23b2b8161ea684cae5
Marius Kleidl <marius@transloadit.com>
2024-11-25 15:44:17 +0100

    Fix formatting

--- src/Transloadit.ts
+++ src/Transloadit.ts
@@ -630,7 +630,8 @@ export class Transloadit {
    * Construct a signed Smart CDN URL. See https://transloadit.com/docs/topics/signature-authentication/#smart-cdn.
    */
   getSignedSmartCDNUrl(opts: SmartCDNUrlOptions): string {
-    if (opts.workspace == null || opts.workspace === '') throw new TypeError('workspace is required')
+    if (opts.workspace == null || opts.workspace === '')
+      throw new TypeError('workspace is required')
     if (opts.template == null || opts.template === '') throw new TypeError('template is required')
     if (opts.input == null) throw new TypeError('input is required') // `input` can be an empty string.
 
