996e1b635698969ddae8e5547007a6ec62147ae7
Kevin van Zonneveld <kevin@vanzonneveld.net>
2024-11-27 12:47:17 +0100

    Update vitest.config.mjs

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -8,9 +8,10 @@ export default defineConfig({
       reporter: ['json', 'lcov', 'text', 'clover', 'json-summary', 'html'],
       provider: 'v8',
       thresholds: {
+        // We want to boost this to 80%, but that should happen in a separate PR
         statements: 2,
         branches: 2,
-        functions: 2,
+        functions: 0,
         lines: 2,
         perFile: true,
       },
