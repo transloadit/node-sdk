7caf09042a349676f1a0a58f604004048b06b27d
Kevin van Zonneveld <vanzonneveld@gmail.com>
2024-12-05 13:08:33 +0100

    Add Codecov (#202)
    * Add comprehensive coverage reporting and thresholds
    
    * Lower thresholds
    
    * Also update in vitest
    
    * Update vitest.config.mjs
    
    * Update vitest.config.mjs
    
    Co-authored-by: Remco Haszing <remcohaszing@gmail.com>
    
    * Update .github/workflows/ci.yml
    
    Co-authored-by: Mikael Finstad <finstaden@gmail.com>
    
    * Update ci.yml
    
    ---------
    
    Co-authored-by: Remco Haszing <remcohaszing@gmail.com>
    Co-authored-by: Mikael Finstad <finstaden@gmail.com>

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,8 +3,18 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      include: 'src',
-      reporter: ['json', 'lcov', 'text', 'clover', 'json-summary'],
+      include: ['src/**/*.ts'],
+      exclude: ['**/*.d.ts', '**/*.test.ts', '**/test/**'],
+      reporter: ['json', 'lcov', 'text', 'clover', 'json-summary', 'html'],
+      provider: 'v8',
+      thresholds: {
+        // We want to boost this to 80%, but that should happen in a separate PR
+        statements: 2,
+        branches: 2,
+        functions: 0,
+        lines: 2,
+        perFile: true,
+      },
     },
     globals: true,
   },
