1a5babe8c031f37fb79d3e9aca6f7eca55141a0b
Mikael Finstad <finstaden@gmail.com>
2025-03-19 14:24:52 +0800

    fix non-functioning linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,27 @@
 module.exports = {
   root: true,
   extends: ['transloadit', 'prettier'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
     requireConfigFile: false,
   },
+  globals: {
+    // https://github.com/Chatie/eslint-config/issues/45#issuecomment-885507652
+    NodeJS: true,
+  },
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.js', '.ts'],
+        extensions: ['.js', '.ts', '.mjs', '.mts'],
       },
     },
   },
   rules: {
     'import/extensions': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
   },
   overrides: [
     {
