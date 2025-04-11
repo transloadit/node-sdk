cbb8f5b70ff3ffd1f9b93de15404360ce89e4c52
Mikael Finstad <finstaden@gmail.com>
2025-04-11 16:33:12 +0200

    Update test/generate-coverage-badge.ts
    Co-authored-by: Remco Haszing <remcohaszing@gmail.com>

--- test/generate-coverage-badge.ts
+++ test/generate-coverage-badge.ts
@@ -3,24 +3,18 @@
 import fs from 'fs/promises'
 import { makeBadge } from 'badge-maker'
 
-try {
-  const json = JSON.parse(await fs.readFile(process.argv[2], 'utf-8'))
+const json = JSON.parse(await fs.readFile(process.argv[2], 'utf-8'))
 
-  // We only care about "statements"
-  const coveragePercent = `${json.total.statements.pct}%`
+// We only care about "statements"
+const coveragePercent = `${json.total.statements.pct}%`
 
-  // https://github.com/badges/shields/tree/master/badge-maker#format
-  const format = {
-    label: 'coverage',
-    message: coveragePercent,
-    color: 'green',
-  }
-
-  const svg = makeBadge(format)
-
-  await fs.writeFile('coverage-badge.svg', svg)
-} catch (err) {
-  // eslint-disable-next-line no-console
-  console.error(err)
-  process.exit(1)
+// https://github.com/badges/shields/tree/master/badge-maker#format
+const format = {
+  label: 'coverage',
+  message: coveragePercent,
+  color: 'green',
 }
+
+const svg = makeBadge(format)
+
+await fs.writeFile('coverage-badge.svg', svg)
